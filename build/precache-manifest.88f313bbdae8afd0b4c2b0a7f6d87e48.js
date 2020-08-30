08ea717daf608c7441e834e5cfb09b93598029a2
chubiboo <43607068+chubiboo@users.noreply.github.com>
2020-08-30 08:05:35 +0100

    Add files via upload

---
+++ build/precache-manifest.88f313bbdae8afd0b4c2b0a7f6d87e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416cfd0ca01cdaecad63339cd355d3df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96f1a15bf618f2f5a26e",
+    "url": "/static/css/main.ea41f4e5.chunk.css"
+  },
+  {
+    "revision": "f07122166bebae62a26b",
+    "url": "/static/js/2.a5ff1992.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5ff1992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f1a15bf618f2f5a26e",
+    "url": "/static/js/main.228b2212.chunk.js"
+  },
+  {
+    "revision": "9531c97d139d905ac5dd",
+    "url": "/static/js/runtime-main.6ef4ad99.js"
+  }
+]);
